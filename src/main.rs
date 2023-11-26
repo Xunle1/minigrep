2d7b56ef9422bb0b1b63de669d9baf56eb13ee35
xunle <xunle1121@gmail.com>
2023-11-26 18:27:14 +0800

    minigrep initialize

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{env, fs, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    let contents = fs::read_to_string(config.file_path).expect("cannot read file");
+
+    println!("query: {}", config.query);
+    println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
